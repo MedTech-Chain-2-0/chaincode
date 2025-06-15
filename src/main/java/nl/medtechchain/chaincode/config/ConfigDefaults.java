2c8e056f4985040d09f2884e766d3b3cf690cb4a
eyarar <E.Yarar@student.tudelft.nl>
2025-06-15 14:50:02 +0300

    fix

--- src/main/java/nl/medtechchain/chaincode/config/ConfigDefaults.java
+++ src/main/java/nl/medtechchain/chaincode/config/ConfigDefaults.java
@@ -97,10 +97,11 @@ public class ConfigDefaults {
             // list.add(entry(CONFIG_FEATURE_QUERY_ENCRYPTION_SCHEME, "bfv"));
             
             // Uncomment to use Paillier instead
-            list.add(entry(CONFIG_FEATURE_QUERY_ENCRYPTION_SCHEME, "paillier"));
+            list.add(entry(CONFIG_FEATURE_QUERY_ENCRYPTION_ACTIVE, "true"));
             var api = PaillierTTPAPI.getInstance(EncryptionDefaults.TTP_ADDRESS);
             try {
                 var key = api.encryptionKey(EncryptionDefaults.BIT_LENGTH);
+                list.add(entry(CONFIG_FEATURE_QUERY_ENCRYPTION_SCHEME, "paillier"));
                 list.add(entry(PlatformConfig.Config.CONFIG_FEATURE_QUERY_ENCRYPTION_PAILLIER_PUBLIC_KEY, key.getEncryptionKey()));
                 list.add(entry(PlatformConfig.Config.CONFIG_FEATURE_QUERY_ENCRYPTION_KEY_VERSION, key.getVersion())); // CONFIG_FEATURE_QUERY_ENCRYPTION_KEY_VERSION = 15
             } catch (IOException | InterruptedException e) {
